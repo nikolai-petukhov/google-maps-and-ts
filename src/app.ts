f5597969fa428a5ff6411277b4a331ed01c3dc05
nikolai-petukhov <nikolaipetukhov@gmai.com>
2022-10-01 14:47:05 +0300

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-const form = document.querySelector("form")!;
-const addressInput = document.getElementById("address")! as HTMLInputElement;
+const form: HTMLFormElement = document.querySelector("form")!;
+const addressInput: HTMLInputElement = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyC7HoM_rVWXhCva2bX-jlXhlsYpsjz9u9o";
+const GOOGLE_API_KEY: string = "AIzaSyC7HoM_rVWXhCva2bX-jlXhlsYpsjz9u9o";
 
 type GoogleGeocodingResponse = {
     results: { geometry: { location: { lat: number; lng: number } } }[];
     status: "OK" | "ZERO_RESULTS";
 };
 
-function searchAddressHandler(event: Event) {
+function searchAddressHandler(event: Event): void {
     event.preventDefault();
-    const enteredAddress = addressInput.value;
+    const enteredAddress: string = addressInput.value;
 
     axios.get<GoogleGeocodingResponse>(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
@@ -24,6 +24,7 @@ function searchAddressHandler(event: Event) {
                 throw new Error("Could not fetch location!");
             }
             const coordinates = response.data.results[0].geometry.location;
+            console.log(coordinates)
             const map = new google.maps.Map(document.getElementById("map")!, {
                 center: coordinates,
                 zoom: 16
